0a042446bb85eaed1706230b0557d1df0554296f
Abhi56790 <153067914+Abhi56790@users.noreply.github.com>
2024-01-14 15:39:31 +0930

    Add files via upload

---
+++ question no 1 task-1.py
@@ -0,0 +1,16 @@
+# This program extracts the text from four csv files and saves to text file named new_file.txt
+
+import csv # library to open .csv file
+
+filenames = ['CSV1.csv', 'CSV2.csv', 'CSV3.csv', 'CSV4.csv']
+
+with open('new_file.txt', 'w') as file1: 
+    for filename in filenames: # iterate through all files
+        with open(filename, 'r') as file2: # to open each .csv file
+            csvreader = csv.reader(file2) # from csv library
+            next(csvreader) # pointer points to first row
+
+            for row in csvreader: # iterate through each row in file
+                for column in row:
+                    file1.write(column) # write text in each column to new file
+                    file1.write("\n")
