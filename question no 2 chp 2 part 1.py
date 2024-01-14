6f0bf18d6040b4c9e3cdca92ccdd0be79e00159d
Akshitpatel181 <150229403+Akshitpatel181@users.noreply.github.com>
2024-01-14 22:55:00 +0930

    Update question no 2 chp 2 part 1.py

--- question no 2 chp 2 part 1.py
+++ question no 2 chp 2 part 1.py
@@ -1,27 +1,37 @@
-# This program finds the ASCII values of the uppercase letters and even numbers of a string
+def separate_and_convert(s):
+    """
+    Separates numbers and letters from the input string and converts even numbers to ASCII values,
+    and upper-case letters to their ASCII values.
 
-s = '56aAww1984sktr235270aYmn145ss785fsq31D0'
+    Parameters:
+    s (str): Input string containing both numbers and letters.
 
-number_string = ""
-letter_string = ""
-ASCII_number = []
-ASCII_letter = []
+    Returns:
+    str: String of even numbers.
+    str: String of upper-case letters.
+    list: ASCII values of even numbers.
+    list: ASCII values of upper-case letters.
+    """
 
-# split characters and numbers
-for char in s:
-    if char.isalpha():
-        letter_string += char
-    else:
-        number_string += char
+    # Separate numbers and letters
+    number_string = ''.join([char for char in s if char.isdigit()])
+    letter_string = ''.join([char for char in s if char.isalpha()])
 
-# ASCII values of the uppercase letters and even numbers
-for char in letter_string:
-    if char.isupper(): # only appending uppercase lettters
-        ASCII_letter.append(ord(char))
+    # Convert even numbers to ASCII Code Decimal Values
+    even_numbers = [int(num) for num in number_string if int(num) % 2 == 0]
+    ascii_values_numbers = [ord(str(num)) for num in even_numbers]
 
-for char in number_string:
-    if int(char) % 2 == 0: # only appending even numbers
-        ASCII_number.append(ord(char))
+    # Convert upper-case letters to ASCII Code Decimal Values
+    upper_case_letters = [char for char in letter_string if char.isupper()]
+    ascii_values_letters = [ord(char) for char in upper_case_letters]
 
-print(ASCII_letter)
-print(ASCII_number)
+    return ''.join(map(str, even_numbers)), ''.join(map(str, upper_case_letters)), ascii_values_numbers, ascii_values_letters
+
+# Example usage
+input_string = 'AKSHITpatel1213'
+result_numbers, result_letters, ascii_numbers, ascii_letters = separate_and_convert(input_string)
+
+print("Number String:", result_numbers)
+print("Letter String:", result_letters)
+print("ASCII Values of Even Numbers:", ascii_numbers)
+print("ASCII Values of Upper-case Letters:", ascii_letters)
