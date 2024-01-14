b89e6f51478543ce67b661490ab7538f7d6a5e93
Abhi56790 <153067914+Abhi56790@users.noreply.github.com>
2024-01-14 22:36:59 +0930

    Update Error fixed code of question no 3.py

--- Error fixed code of question no 3.py
+++ Error fixed code of question no 3.py
@@ -1,46 +1,45 @@
+
+#Error fixed code of question no 3
+
 global_variable = 100
 my_dict = {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}
 
 def process_numbers():
-   global global_variable
-   local_variable = 5
-   numbers = [1, 2, 3, 4, 5]
+    global global_variable   #Declare global_variable as global
+    local_variable = 5
+    numbers = [1, 2, 3, 4, 5]
 
-   while local_variable > 0:
-       if local_variable % 2 == 0:
-           numbers.remove(local_variable)
-       local_variable -= 1
+    while local_variable > 0:   #Fix the indentation
+        if local_variable % 2 == 0:
+            numbers.remove(local_variable)
+        local_variable -= 1
 
-   return numbers
+    return numbers
 
 m1_set = {1, 2, 3, 4, 5, 5, 4, 3, 2, 1}
-result = process_numbers(numbers=my_set)
-
-
+result = process_numbers()   #Remove the argument, numbers, as it is not used
 
 def modify_dict():
-   local_variable = 10
-   m1_dicty ['ke 4'] = local_variable 
+    local_variable = 10
+    my_dict['key4'] = local_variable   #Correct the dictionary name
 
-modif1_dict(5)
-
-
-
+modify_dict()   #Remove the argument, as it is not used
 
 def update_global():
-   global global_variable
-   global_variable += 10
+    global global_variable  # Declare global_variable as global
+    global_variable += 10
 
 for i in range(5):
-   print(i)
-   i += 1
+    print(i)
+    i += 1   #This change has no effect since 'i' is reassigned in the next iteration
 
-if my_set is not None and my_dict['key4'] == 10:
-   print("Condition met!")
+if my_dict['key4'] == 10:   #Correct the dictionary name
+    print("Condition met!")
 
-if 5 not in m1_dict:
-   print("5 not found in the dictionary!")
+if 5 not in my_dict:  # Correct the dictionary name
+    print("5 not found in the dictionary!")
 
 print(global_variable)
 print(my_dict)
+my_set = {1, 2, 3, 4, 5}   #Reset my_set to the correct value
 print(my_set)
